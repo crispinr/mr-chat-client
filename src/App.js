ba8aece6b6d981de5c9fca71ad3c290ee5f02d53
Crispin R <crispinr003@gmail.com>
2022-01-12 00:46:54 +0530

    Update server address

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const backend_address = "http://localhost:3001";
+const backend_address = "https://mr-chat-server.herokuapp.com/";
 
 const socket = io.connect(backend_address);
 
