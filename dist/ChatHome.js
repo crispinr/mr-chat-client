be68dda7bbbe05afeaecd0da70de3c1459cf2868
Crispin R <crispinr003@gmail.com>
2022-01-16 23:21:30 +0530

    Update with babel

--- dist/ChatHome.js
+++ dist/ChatHome.js
@@ -2,11 +2,8 @@ import "./Chat.css";
 import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
-
 const backend_address = "https://mr-chat-server.herokuapp.com/";
-
 const socket = io.connect(backend_address);
-
 export default function ChatHome() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
@@ -19,33 +16,30 @@ export default function ChatHome() {
     }
   };
 
-  return (
-    <>
-      {!showChat ? (
-        <div className="joinChatContainer">
-          <h3 className="header">Join a Chat Room</h3>
-          <input
-            type="text"
-            placeholder="Name"
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Room Key"
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-            onKeyPress={(event) => {
-              event.key === "Enter" && joinRoom();
-            }}
-          />
-          <button onClick={joinRoom}>Join</button>
-        </div>
-      ) : (
-        <Chat socket={socket} username={username} room={room} />
-      )}
-    </>
-  );
+  return /*#__PURE__*/React.createElement(React.Fragment, null, !showChat ? /*#__PURE__*/React.createElement("div", {
+    className: "joinChatContainer"
+  }, /*#__PURE__*/React.createElement("h3", {
+    className: "header"
+  }, "Join a Chat Room"), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    placeholder: "Name",
+    onChange: event => {
+      setUsername(event.target.value);
+    }
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    placeholder: "Room Key",
+    onChange: event => {
+      setRoom(event.target.value);
+    },
+    onKeyPress: event => {
+      event.key === "Enter" && joinRoom();
+    }
+  }), /*#__PURE__*/React.createElement("button", {
+    onClick: joinRoom
+  }, "Join")) : /*#__PURE__*/React.createElement(Chat, {
+    socket: socket,
+    username: username,
+    room: room
+  }));
 }
