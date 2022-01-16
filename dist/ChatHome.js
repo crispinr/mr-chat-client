cf46d2e85c57c2f0208aa336494dbc8bc0e41b33
Crispin R <crispinr003@gmail.com>
2022-01-16 23:18:09 +0530

    Add new home file

---
+++ dist/ChatHome.js
@@ -0,0 +1,51 @@
+import "./Chat.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+const backend_address = "https://mr-chat-server.herokuapp.com/";
+
+const socket = io.connect(backend_address);
+
+export default function ChatHome() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <>
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3 className="header">Join a Chat Room</h3>
+          <input
+            type="text"
+            placeholder="Name"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room Key"
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+            onKeyPress={(event) => {
+              event.key === "Enter" && joinRoom();
+            }}
+          />
+          <button onClick={joinRoom}>Join</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </>
+  );
+}
